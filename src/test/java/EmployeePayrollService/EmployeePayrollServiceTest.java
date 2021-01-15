c7907e76b772dea968ad4de75f55e5a6f852ddf4
Komal-8080 <shindeg163@gmail.com>
2021-01-16 01:09:43 +0530

    Added: Watch Service To Watch Particular Directory Along With All Files and Sub-Directories

--- src/test/java/EmployeePayrollService/EmployeePayrollServiceTest.java
+++ src/test/java/EmployeePayrollService/EmployeePayrollServiceTest.java
@@ -15,21 +15,21 @@ public class EmployeePayrollServiceTest {
 
 	@Test
 	public void givenPathWhenCheckedThenConfirm() throws IOException {
-		
+
 		// Checks file exists
 		Path homePath = Paths.get(HOME);
 		Assert.assertTrue(Files.exists(homePath));
-		
+
 		// Deletes files checks file not exists
 		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
 		if (Files.exists(playPath))
 			FileUtils.deleteFiles(playPath.toFile());
 		Assert.assertTrue(Files.notExists(playPath));
-		
+
 		// create Directory
 		Files.createDirectories(playPath);
 		Assert.assertTrue(Files.exists(playPath));
-		
+
 		// Creates file
 		IntStream.range(1, 10).forEach(cntr -> {
 			Path tempFile = Paths.get(playPath + "/temp" + cntr);
@@ -40,12 +40,19 @@ public class EmployeePayrollServiceTest {
 			}
 			Assert.assertTrue(Files.exists(tempFile));
 		});
-		
+
 		// lists directories and files with extension
 		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
 		Files.newDirectoryStream(playPath).forEach(System.out::println);
 		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
 				.forEach(System.out::println);
 		;
+	}
+
+	@Test
+	public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException {
+		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
 	}
 }
