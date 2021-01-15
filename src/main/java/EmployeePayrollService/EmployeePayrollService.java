6a1a51f3f54adcc4dbfd754b7792cf1e9b892346
Komal-8080 <shindeg163@gmail.com>
2021-01-16 01:34:25 +0530

    Added: Employee Payroll Service to Store Employee Payroll Into A File

--- src/main/java/EmployeePayrollService/EmployeePayrollService.java
+++ src/main/java/EmployeePayrollService/EmployeePayrollService.java
@@ -5,21 +5,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-	
-	//Enum method to put constants
+
+	// Enum method to put constants
 	public enum IOService {
 		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
 	}
 
-	//Array list to put employee details
+	// Array list to put employee details
 	private List<EmployeePayrollData> employeePayrollList;
-	
-	//Employee pay roll service Constructor
+
+	// Employee pay roll service Constructor
 	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
 		this.employeePayrollList = employeePayrollList;
 	}
 
-	//Read method to take employee data from console 
+	// Read method to take employee data from console
 	private void readEmployeePayrollData(Scanner consoleInputReader) {
 		System.out.print("Enter Employee ID: ");
 		int id = consoleInputReader.nextInt();
@@ -30,17 +30,18 @@ public class EmployeePayrollService {
 		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
 	}
 
-	//Write method to print employee data on console
-	private void writeEmployeePayrollData() {
-		System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+	// Write method to print employee data on console
+	public void writeEmployeePayrollData(IOService ioService) {
+		if (ioService.equals(IOService.CONSOLE_IO))
+			System.out.println("\nWriting Payroll to Console\n" + employeePayrollList);
+		else if (ioService.equals(IOService.FILE_IO))
+			new EmployeePayrollFileIOService().writeData(employeePayrollList);
+
 	}
 
-	//Main method
-	public static void main(String[] args) {
-		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-		Scanner consoleInputReader = new Scanner(System.in);
-		employeePayrollService.readEmployeePayrollData(consoleInputReader);
-		employeePayrollService.writeEmployeePayrollData();
+	public long countEntries(IOService ioService) {
+		if (ioService.equals(IOService.FILE_IO))
+			return new EmployeePayrollFileIOService().countEntries();
+		return 0;
 	}
 }
--- src/test/java/EmployeePayrollService/EmployeePayrollServiceTest.java
+++ src/test/java/EmployeePayrollService/EmployeePayrollServiceTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 import org.junit.Assert;
 import org.junit.Test;
@@ -54,5 +55,16 @@ public class EmployeePayrollServiceTest {
 		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
 		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
 		new Java8WatchServiceExample(dir).processEvents();
+	}
+
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchNumberOfEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Jeff Becos", 100000.0),
+											new EmployeePayrollData(2, "Bill Gates", 200000.0),
+											new EmployeePayrollData(3, "Mark ZukerBerg", 300000.0) };
+		EmployeePayrollService empPayrollService;
+		empPayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		empPayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+		Assert.assertEquals(3, empPayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO));
 	}
 }
